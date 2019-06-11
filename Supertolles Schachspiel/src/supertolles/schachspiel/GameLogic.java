f9764baff75139d0d93c27ae1bb8f09bc52d43e6
Cyention <>
2019-06-11 17:44:37 +0200

    Startposition in GameLogic geändert, sodass die Figuren in Zeilen und nicht Spalten stehen

--- Supertolles Schachspiel/src/supertolles/schachspiel/GameLogic.java
+++ Supertolles Schachspiel/src/supertolles/schachspiel/GameLogic.java
@@ -22,32 +22,31 @@ public class GameLogic {
         
         startPosition();
         printBoard();
-        flipBoard();
     }
     
     public void startPosition(){
         board[0][0] = new Piece(0, 0, "black", "Rook");
-        board[0][boardlength-1] = new Piece(0, boardlength-1, "black", "Rook");
-        board[0][1] = new Piece(0, 1, "black", "Knight");
-        board[0][boardlength-2] = new Piece(0, boardlength-2, "black", "Knight");
-        board[0][2] = new Piece(0, 2, "black", "Bishop");
-        board[0][boardlength-3] = new Piece(0, boardlength-3, "black", "Bishop");
-        board[0][3] = new Piece(0, 3, "black", "Queen");
-        board[0][4] = new Piece(0, 4, "black", "King");
+        board[boardlength-1][0] = new Piece(0, boardlength-1, "black", "Rook");
+        board[1][0] = new Piece(0, 1, "black", "Knight");
+        board[boardlength-2][0] = new Piece(0, boardlength-2, "black", "Knight");
+        board[2][0] = new Piece(0, 2, "black", "Bishop");
+        board[boardlength-3][0] = new Piece(0, boardlength-3, "black", "Bishop");
+        board[3][0] = new Piece(0, 3, "black", "Queen");
+        board[4][0] = new Piece(0, 4, "black", "King");
         for (int i = 0; i < boardlength; i++) {
-            board[1][i] = new Piece(1, i, "black", "Pawn");
+            board[i][1] = new Piece(1, i, "black", "Pawn");
         }
         
-        board[boardlength-1][0] = new Piece(boardlength-1, 0, "white", "Rook");
+        board[0][boardlength-1] = new Piece(boardlength-1, 0, "white", "Rook");
         board[boardlength-1][boardlength-1] = new Piece(boardlength-1, boardlength-1, "white", "Rook");
-        board[boardlength-1][1] = new Piece(boardlength-1, 1, "white", "Knight");
-        board[boardlength-1][boardlength-2] = new Piece(boardlength-1, boardlength-2, "white", "Knight");
-        board[boardlength-1][2] = new Piece(boardlength-1, 2, "white", "Bishop");
-        board[boardlength-1][boardlength-3] = new Piece(boardlength-1, boardlength-3, "white", "Bishop");
-        board[boardlength-1][3] = new Piece(boardlength-1, 3, "white", "Queen");
-        board[boardlength-1][4] = new Piece(boardlength-1, 4, "white", "King");
+        board[1][boardlength-1] = new Piece(boardlength-1, 1, "white", "Knight");
+        board[boardlength-2][boardlength-1] = new Piece(boardlength-1, boardlength-2, "white", "Knight");
+        board[2][boardlength-1] = new Piece(boardlength-1, 2, "white", "Bishop");
+        board[boardlength-3][boardlength-1] = new Piece(boardlength-1, boardlength-3, "white", "Bishop");
+        board[3][boardlength-1] = new Piece(boardlength-1, 3, "white", "Queen");
+        board[4][boardlength-1] = new Piece(boardlength-1, 4, "white", "King");
         for (int i = 0; i < boardlength; i++) {
-            board[boardlength-2][i] = new Piece(boardlength-2, i, "white", "Pawn");
+            board[i][boardlength-2] = new Piece(boardlength-2, i, "white", "Pawn");
         }   
     }
     
